83295d722346791437cd54c1fb55110f7092523b
Qiang <qiang.li@cern.ch>
2016-11-02 09:12:23 +0100

    Add GEM Hit and Segment Analyzers

--- RecoLocalMuon/GEMRecHit/test/testgemrechitanalyzer_cfg.py
+++ RecoLocalMuon/GEMRecHit/test/testgemrechitanalyzer_cfg.py
@@ -7,19 +7,19 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 # 10 eta partitions :: command line option :: --geometry Geometry/GEMGeometry/cmsExtendedGeometryPostLS1plusGEMr10v01XML_cfi.py
 # process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMr08v01XML_cfi')
 # process.load('Geometry.GEMGeometry.cmsExtendedGeometryPostLS1plusGEMr10v01XML_cfi')
-process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDevReco_cff')
-process.load('Configuration.Geometry.GeometryExtended2015MuonGEMDev_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D1_cff')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:out_local_reco.root'
+        'file:/afs/cern.ch/user/q/qili/workspace/GEMTEST/201611/pre10/sample/src/singlemugun.root'
     )
 )
 
-process.demo = cms.EDAnalyzer('TestGEMRecHitAnalyzer',
+process.demo = cms.EDAnalyzer('MyGEMRecHitAnalyzer',
                               RootFileName = cms.untracked.string("TestGEMRecHitHistograms.root"),
 )
 
